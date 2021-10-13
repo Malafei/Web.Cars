91539ebc07c8d929de317c40791bf047441c38e2
MaxMalafei <ukrainelove8@gmail.com>
2021-10-14 00:05:20 +0300

    прилітає в базу (не працює)

--- Controllers/AcountController.cs
+++ Controllers/AcountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Web.Cars.Data.Identity;
@@ -25,6 +26,28 @@ namespace Web.Cars.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromForm]RegisterVievModel model)
         {
+            var user = await _userManager.FindByEmailAsync(model.Email); // шукаєм чи є такій логін в базі
+            if (user != null) // якщо юзер не дорівнює налл це означає що в базі є такий користувач і наш емаіл не буде універсальний
+                return ValidationProblem();
+
+            var ext = Path.GetExtension(model.Photo.FileName); //отримуємо формат файлу
+            string fileName = Path.GetRandomFileName() + ext; //генеруєм рандом імя та додає формат
+
+            var dir = Path.Combine(Directory.GetCurrentDirectory(), "images"); //витягуємо шлях
+            var filePath = Path.Combine(dir, fileName); // комбінуєм шлях і назву файла
+            using (var stream = System.IO.File.Create(filePath)) { model.Photo.CopyTo(stream); } // кідаєм в поток і записуєм у папку
+
+
+            user = new AppUser
+            {
+                Email = model.Email,
+                Login = model.Login,
+                UserName = model.Login,
+                Photo = fileName
+
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
 
             return Ok();
         }
